eb119e641509a9972a25c89b2e3f45a27fb0f74a
falkTX <falktx@gmail.com>
2016-12-14 16:01:22 +0100

    Add custom mod-host link code

---
+++ src/link/mod-link.cpp
@@ -0,0 +1,97 @@
+/*
+ * This file is part of mod-host.
+ *
+ * mod-host is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * mod-host is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with mod-host.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "mod-link.h"
+
+#include "AudioEngine.hpp"
+#include "ableton/link/HostTimeFilter.hpp"
+#include <chrono>
+
+class LinkTransport {
+public:
+    LinkTransport(double bpm, double bufferSize, double sampleRate)
+        : link(bpm),
+          engine(link),
+          outputLatency(0),
+          sampleTime(0)
+    {
+        outputLatency = std::chrono::microseconds(llround(1.0e6 * bufferSize / sampleRate));
+
+        // testing
+        setEnabled(true);
+    }
+
+    void setEnabled(bool enabled)
+    {
+        link.enable(enabled);
+    }
+
+    void setTempo(double tempo)
+    {
+        engine.setTempo(tempo);
+    }
+
+    void process(uint32_t frames, LinkTimeInfo* info)
+    {
+        const auto hostTime = hostTimeFilter.sampleTimeToHostTime(sampleTime);
+
+        sampleTime += frames;
+
+        const auto bufferBeginAtOutput = hostTime + outputLatency;
+
+        engine.audioCallback(bufferBeginAtOutput, info);
+    }
+
+private:
+    ableton::Link link;
+    ableton::linkaudio::AudioEngine engine;
+
+    ableton::link::HostTimeFilter<ableton::platforms::stl::Clock> hostTimeFilter;
+    std::chrono::microseconds outputLatency;
+    double sampleTime;
+};
+
+link_t* link_create(double bpm, uint32_t buffer_size, uint32_t sample_rate)
+{
+    LinkTransport* t;
+
+    try {
+        t = new LinkTransport(bpm, buffer_size, sample_rate);
+    } catch (...) {
+        return nullptr;
+    }
+
+    return (link_t*)t;
+}
+
+void link_set_tempo(link_t* link, double bpm)
+{
+    ((LinkTransport*)link)->setTempo(bpm);
+}
+
+void link_process(link_t* link, uint32_t frames, link_time_info_t* info)
+{
+    ((LinkTransport*)link)->process(frames, (LinkTimeInfo*)info);
+}
+
+void link_cleanup(link_t* link)
+{
+    delete (LinkTransport*)link;
+}
+
+#include "AudioEngine.cpp"
---
+++ src/link/mod-link.h
@@ -0,0 +1,43 @@
+/*
+ * This file is part of mod-host.
+ *
+ * mod-host is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * mod-host is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with mod-host.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef MOD_LINK_H_INCLUDED
+#define MOD_LINK_H_INCLUDED
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct LINK_T link_t;
+
+typedef struct LINK_TIME_INFO_T {
+    double bpm, beats, phase;
+} link_time_info_t;
+
+link_t* link_create(double bpm, uint32_t buffer_size, uint32_t sample_rate);
+void link_process(link_t* link, uint32_t frames, link_time_info_t* info);
+void link_set_tempo(link_t* link, double tempo);
+void link_cleanup(link_t* link);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // MOD_LINK_H_INCLUDED
